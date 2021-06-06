b85623e0bcff954642e6b02b3dc9acecd3d8fa8e
Hasan <hasanuzzaman.dev@gmail.com>
2021-06-06 15:36:41 +0600

    image and assets

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,16 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text("I am Rich"),
+          title: Text('Home'),
           backgroundColor: Colors.blueGrey[900],
           elevation: 0.0,
         ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://filedn.com/ltOdFv1aqz1YIFhf4gTY8D7/ingus-info/BLOGS/Photography-stocks3/stock-photography-slider.jpg'),
+            image: AssetImage('assets/images/diamond.png'),
+            /*image: NetworkImage(
+              'https://filedn.com/ltOdFv1aqz1YIFhf4gTY8D7/ingus-info/BLOGS/Photography-stocks3/stock-photography-slider.jpg',
+            ),*/
           ),
         ),
       ),
